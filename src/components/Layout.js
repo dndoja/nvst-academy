05d60eef626d994962adf4b93c73f850c7360d8b
dndoja <ndoja.damian@gmail.com>
2020-02-02 14:47:05 +0100

    Add navbar in all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
 
-const TemplateWrapper = ({ children }) => {
+const TemplateWrapper = ({ children}) => {
   const { title, description } = useSiteMetadata()
   return (
     <div>
