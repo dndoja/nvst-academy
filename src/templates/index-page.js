117035b606cab346a8aff9c0e68a114231cd0b5f
dndoja <ndoja.damian@gmail.com>
2020-02-07 18:28:20 +0100

    ds

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -59,12 +59,6 @@ export const IndexPageTemplate = ({
       <div className="container">
         <div className="section">
           <div className="columns">
-            <div className="column is-7 is-offset-1">
-              <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
-              <p>{description}</p>
-            </div>
-          </div>
-          <div className="columns">
             <div className="column is-10 is-offset-1">
               <Features gridItems={intro.blurbs} />
               <div className="columns">
